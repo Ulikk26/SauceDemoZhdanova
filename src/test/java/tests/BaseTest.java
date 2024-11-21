372797bab542b26254ea64d4a92f0b9113785389
Zhulik <ulikk@bk.ru>
2024-11-21 10:32:51 +0300

    added retry and annotation

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,11 +3,14 @@ package tests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestListener;
+import org.testng.annotations.*;
 import pages.*;
 
 import java.time.Duration;
+
 
 public class BaseTest {
     WebDriver driver;
@@ -17,11 +20,18 @@ public class BaseTest {
     CheckoutPage checkoutPage;
     CheckoutOverviewPage checkoutOverviewPage;
 
+    @Parameters({"browser"})
     @BeforeMethod
-    public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maxinized");
-        driver = new ChromeDriver(options);
+    public void setUp(@Optional("chrome") String browser) {
+        if(browser.equalsIgnoreCase("chrome")){
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("start-maximized");
+            driver = new ChromeDriver(options);
+        } else if(browser.equalsIgnoreCase("fireFox")){
+            driver = new FirefoxDriver();
+        }else if(browser.equalsIgnoreCase("edge")) {
+            driver = new EdgeDriver();
+        }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         loginPage=new LoginPage(driver);
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,24 +1,26 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
 
 public class LoginTest extends BaseTest {
 
-    @Test
+    //запуск теста повторно при его падении retryAnalyzer = Retry.class
+    @Test(testName = "Проверка позитивного логина", description = "Проверка позитивного логина", retryAnalyzer = Retry.class, priority = 4)
     public void checkLogin(){
         loginPage.open();
-        loginPage.login("standard_user", "secret_sauce");
+        loginPage.login("standard_user", "secret_sauce1");
         assertEquals(
                 productsPage.getTitle(),
                 "Products",
                 "переход на страницу не выполнен");
     }
 
-    @Test
+    //тест временно отключён enabled = false
+    @Test(testName = "Проверка ошибки, при пустом логине", description = "Проверка ошибки, при пустом логине", enabled = false,priority = 3)
     public void checkLoginEmptyUserName(){
         loginPage.open();
         loginPage.login("", "secret_sauce");
@@ -29,7 +31,8 @@ public class LoginTest extends BaseTest {
         );
     }
 
-    @Test
+    //тест запуститься вторым по очередности priority = 2
+    @Test(testName = "Проверка ошибки, при пустом пароля", description = "Проверка ошибки, при пустом пароля", priority = 2)
     public void checkLoginEmptyPassword(){
         loginPage.open();
         loginPage.login("standard_user", "");
@@ -39,7 +42,8 @@ public class LoginTest extends BaseTest {
                 "сообщение об ошибке неверное");
     }
 
-    @Test
+    //тест запуститься первым по очередности priority = 1
+    @Test(testName = "Проверка ошибки, при неверном логине", description = "Проверка ошибки, при неверном логине", priority = 1)
     public void checkLoginEmptyWrongUserName(){
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.id("user-name")).sendKeys("standard_user1");
@@ -47,5 +51,25 @@ public class LoginTest extends BaseTest {
         driver.findElement(By.id("login-button")).click();
         String title= driver.findElement(By.xpath("//h3[@data-test='error']")).getText();
         assertEquals(title, "Epic sadface: Username and password do not match any user in this service", "ошибка не верная");
+    }
+
+    @DataProvider(name = "LoginData")
+    public Object[][] loginDta() {
+        return new Object[][]{
+                {"", "secret_sauce", "Epic sadface: Username is required"},
+                {"standard_user", "", "Epic sadface: Password is required"},
+                {"standard_user1", "secret_sauce", "Epic sadface: Username and password do not match any user in this service"},
+
+        };
+    }
+
+    @Test(dataProvider = "LoginData")
+    public void test(String user, String password, String expectedError) {
+        loginPage.open();
+        loginPage.login(user, password);
+        String errorMassage = loginPage.getErrorMessage();
+        assertEquals(loginPage.getErrorMessage(),
+                expectedError,
+                "сообщение об ошибке неверное");
     }
 }
